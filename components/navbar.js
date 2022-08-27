d43c86ba26b4282108a1ce105c8d8ae6087daee1
isky27 <gaganjindal50@gmail.com>
2022-08-27 07:25:05 +0530

    day-4 completed

---
+++ components/navbar.js
@@ -0,0 +1,69 @@
+function navbar(){
+    return ` <div id="navBar_div">
+      <div id="main_navBar_div">
+        <div id="logo-div">
+          <img
+            src="https://i.postimg.cc/KcPQzkf4/output-onlinepngtools.png"
+            alt=""
+          />
+        </div>
+        <div id="nev_menu_link">
+          <a href="features.html">Features</a>
+          <a href="pricing.html">Pricing</a>
+          <div tabindex="0" class="menu" id="resources_div">
+          <p>Resources <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></p>
+            <div class="menu-dropdown">
+              <a href=""><h3>Blog</h3><p>Read the industry's latest thoughts on digital marketing, content strategy, SEO, PPC, social media and more.</p></a>
+              <a href=""><h3>Help Center</h3><p>Learn how to use Semrush with user manuals, how-to's, videos and more!</p></a>
+              <a href=""><h3>What's New</h3><p>Keep track of the newest Semrush features and improvements.</p></a>
+              <a href=""><h3>Webinars</h3><p>Register and take part in educational webinars conducted by the best digital marketing experts.</p></a>
+              <a href=""><h3>Ebooks</h3><p>A range of downloadable guides in PDF format that can help you broaden your knowledge and improve your skills.</p></a>
+              <a href=""><h3>Hire a Trusted Agency</h3><p>Pressed for time? Need rare skills? Get help from a trusted agency. Our experts work with marketing projects of all kinds and budgets.</p></a>
+              <a href=""><h3>Academy</h3><p>Get vital SEO skills, learn how to use our toolkits and get official certificates of your proficiency in SEO and Semrush.</p></a>
+              <a href=""><h3>Top Websites</h3><p>Discover the most visited websites. Analyze their traffic and search rankings. Choose country or industry to find out who currently leads the market.</p></a>
+            </div>
+          </div>
+          <div tabindex="0" class="menu" id="company_div">
+                 <p>Company <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></p>
+            <div class="menu-dropdown">
+              <a href=""><h3>About Us</h3></a>
+              <a href=""><h3>Newsroom</h3></a>
+               <a href=""><h3>Careers <span><i class="fa fa-external-link" style="font-size:10px;font-weight:bolder;"></i></span></h3></a>
+              <a href=""><h3>Success Stories</h3></a>
+              <a href=""><h3>Data Studies</h3></a>
+              <a href=""><h3>Affilliate Program <span><i class="fa fa-external-link" style="font-size:10px;font-weight:bolder;"></i></span></h3></a>
+              <a href=""><h3>For Investors <span><i class="fa fa-external-link" style="font-size:10px;font-weight:bolder;"></i></span></h3></a>
+              <a href=""><h3>Contacts</h3></a>
+              <a href=""><h3>Stats and facts</h3></a>
+
+            </div>
+          </div>
+          <div tabindex="0" class="menu" id="appCenter_div">
+          <p>App Center <span><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAALCAYAAAB/Ca1DAAAABmJLR0QA/wD/AP+gvaeTAAAACW9GRnMAAAAAAAAAFgDGsgMZAAAAEGNhTnYAAAAUAAAACwAAAAAAAAAWUQnAbAAAAEJJREFUKM9j9F9n/J+BioCJmobR3sANgWfgGJcYLhoGWNBtCFhvAlcIY8NoqnkZ3dX4AAsximjuQuTwQ7eMceSlQwDBRidGVtcaCAAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAAASUVORK5CYII=" alt=""></span><span><i class="fa fa-angle-down" aria-hidden="true"></i></span></p>
+            <div class="menu-dropdown">
+              <a href=""><h3>Store</h3></a>
+              <a href=""><h3>My Apps</h3></a>
+              <a href=""><h3>About</h3></a>
+              <a href=""><h3>For Partners</h3></a>
+             </div>
+          </div>
+            <div tabindex="0" class="menu" id="extraTools_div">
+            <p>Extra tools <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></p>
+              <div class="menu-dropdown">
+              <a href=""><h3>Content Marketplace</h3></a>
+              <a href=""><h3>Content Outline Builder <span><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAALCAYAAACUPhZAAAAABmJLR0QA/wD/AP+gvaeTAAAACW9GRnMAAAAAAAAACwA53m8FAAAAEGNhTnYAAAAXAAAACwAAAAAAAAALj8XAewAAAFZJREFUKM9j/F/P8J+BRoCJVgbT3HAWFF4DWgg1MKKKYeOj64OJYXV5AyOKAgwxDPo/dj0YLkd2BbJibGL4fIzbcDQX4TMUX/DgdTkh3xABGEfTOTYAAIt0IJ7IVCAKAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAABJRU5ErkJggg==" alt=""></span></h3></a>
+              <a href=""><h3>ImpactHero</h3></a>
+              <a href=""><h3>Tools for Amazon</h3></a>
+              <a href=""><h3>SplitSignal <span><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAALCAYAAACUPhZAAAAABmJLR0QA/wD/AP+gvaeTAAAACW9GRnMAAAAAAAAACwA53m8FAAAAEGNhTnYAAAAXAAAACwAAAAAAAAALj8XAewAAAFZJREFUKM9j/F/P8J+BRoCJVgbT3HAWFF4DWgg1MKKKYeOj64OJYXV5AyOKAgwxDPo/dj0YLkd2BbJibGL4fIzbcDQX4TMUX/DgdTkh3xABGEfTOTYAAIt0IJ7IVCAKAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAABJRU5ErkJggg==" alt=""></span></h3></a>
+              <a href=""><h3>Custom reports</h3></a>
+              <a href=""><h3>Surround Sound <span><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAALCAYAAACUPhZAAAAABmJLR0QA/wD/AP+gvaeTAAAACW9GRnMAAAAAAAAACwA53m8FAAAAEGNhTnYAAAAXAAAACwAAAAAAAAALj8XAewAAAFZJREFUKM9j/F/P8J+BRoCJVgbT3HAWFF4DWgg1MKKKYeOj64OJYXV5AyOKAgwxDPo/dj0YLkd2BbJibGL4fIzbcDQX4TMUX/DgdTkh3xABGEfTOTYAAIt0IJ7IVCAKAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAABJRU5ErkJggg==" alt=""></span></h3></a>
+              <a href=""><h3>Pagelmprove <span><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAALCAYAAACUPhZAAAAABmJLR0QA/wD/AP+gvaeTAAAACW9GRnMAAAAAAAAACwA53m8FAAAAEGNhTnYAAAAXAAAACwAAAAAAAAALj8XAewAAAFZJREFUKM9j/F/P8J+BRoCJVgbT3HAWFF4DWgg1MKKKYeOj64OJYXV5AyOKAgwxDPo/dj0YLkd2BbJibGL4fIzbcDQX4TMUX/DgdTkh3xABGEfTOTYAAIt0IJ7IVCAKAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAABJRU5ErkJggg==" alt=""></span></h3></a>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+    `
+}
+
+export default navbar
